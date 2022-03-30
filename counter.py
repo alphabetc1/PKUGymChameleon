0e9e2b7bb5ea4f15c8ae3a9b9c271e98be127db2
alphabetc1 <2508695655@qq.com>
2022-03-30 22:11:31 +0800

    [feat] Support boot gym

--- counter.py
+++ counter.py
@@ -6,13 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.common.by import By
 import datetime
+import time
+import random
 
-def get_boot_date():
+def get_boot_date(st, ed):
     date = []
     today = datetime.datetime.now()
     offset = datetime.timedelta(days=1)
 
-    for i in range(3):
+    for i in range(ed, st - 1, -1):
         boot_day = (today + i * offset).strftime("%Y-%m-%d")
         date.append(boot_day)
 
@@ -34,16 +36,16 @@ class Counter:
         while bootNum == 0 :
             try:
                 self.login()
-                self.select_and_boot()
-                if self.conf.boot:
-                    bootNum = self.boot()
-                    if bootNum != 0 & self.conf.wechat:
-                        self.wechat_notification()
+                bootNum += self.select_and_boot()
+                if bootNum != 0 :
+                    break
+                time.sleep(300 + random.random() * 300)
 
             except Exception as e:
                 print('Error...')
                 print(e)
                 self.driver.quit()
+                break
                 
 
     def login(self, retry = 0):
@@ -74,32 +76,64 @@ class Counter:
         
     def select_and_boot(self):
         self.select()
+        print ('Booting...')
+        # times = [19, 20, 21, 15, 16, 17, 18, 11, 10]
+        cnt = 0
         
-        boot_date = get_boot_date()
+        boot_date = get_boot_date(self.conf.date_st, self.conf.date_ed)
+ 
         for date in boot_date:
             self.driver.find_element(By.CLASS_NAME, 'ivu-icon-ios-calendar-outline').click()
             self.driver.find_element(By.CLASS_NAME, 'ivu-input-with-suffix').send_keys(date)
             self.driver.find_element(By.CLASS_NAME, 'ivu-icon-md-refresh').click()
+            page = 0
 
-            while True:
+            while cnt == 0:
                 # reserve
-                WebDriverWait(self.driver, 3).until(EC.visibility_of(self.driver.find_element(By.CLASS_NAME, 'reserveBtn')))
-                self.driver.find_element(By.CLASS_NAME, 'reserveBtn').click()
-                
+                index = 0
+                WebDriverWait(self.driver, 3).until(lambda _: len(self.driver.find_elements(By.CLASS_NAME, 'reserveBlock'))>=28)
+                reserve_blocks = self.driver.find_elements(By.CLASS_NAME, 'reserveBlock')
+                row = 2 if page == 2 else 5
+
+                # for time in self.timesPriority:
+                for timeStr in self.conf.timesPriority:
+                    time = int(timeStr)
+                    start_index = (time - 8) * row
+                    for delta in range(row - 1, -1, -1):
+                        if reserve_blocks[start_index + delta].get_attribute('class').find('free') != -1:
+                            reserve_blocks[start_index + delta].click()
+                            self.driver.find_element(By.CLASS_NAME, 'ivu-checkbox-input').click()
+                            self.driver.find_elements(By.CLASS_NAME, 'payHandleItem')[1].click()
+                            WebDriverWait(self.driver, 3).until(lambda _:len(self.driver.find_elements(By.CLASS_NAME, 'ivu-input-default'))==5)
+                            self.driver.find_elements(By.CLASS_NAME, 'ivu-input-default')[0].\
+                                    clear()
+                            self.driver.find_elements(By.CLASS_NAME, 'ivu-input-default')[0].\
+                                    send_keys(self.conf.phone_number)
+                            self.driver.find_elements(By.CLASS_NAME, 'payHandleItem')[1].click()
+                            WebDriverWait(self.driver, 3).until(lambda _:len(self.driver.find_elements(By.CLASS_NAME, 'cardPay'))>0)
+                            self.driver.find_elements(By.CLASS_NAME, 'cardPay')[0].click()
+                            self.driver.find_elements(By.CLASS_NAME, 'ivu-btn')[1].click()
+                            cnt += 1
+                            print('日期:' + date + ' page:' + str(page) + ' 时间:' + str(time) + ' 场地' + str(page * 5 + delta + 1))
+                    if cnt != 0:
+                        break
 
                 # pull right
                 pull_right = len(self.driver.find_elements(By.CLASS_NAME, 'pull-right'))>0
                 if pull_right is False:
                     break
+                page += 1
                 self.driver.find_element(By.CLASS_NAME, 'pull-right').click()
 
-        return 0
+        return cnt
 
 
     def select(self):
         print('Selecting...')
-        self.driver.find_element(By.CLASS_NAME, 'tabWrap').\
-        find_elements(By.CLASS_NAME, 'tabItem')[1].click()
+        self.driver.find_element(By.CLASS_NAME, 'homeWrap').\
+            find_element(By.CLASS_NAME, 'header').\
+                find_element(By.CLASS_NAME, 'headerContent').\
+                    find_elements(By.CLASS_NAME, 'tabItem')[1].click()
         WebDriverWait(self.driver, 3).until(EC.url_to_be('https://epe.pku.edu.cn/venue/pku/venue-introduce?selectIndex=1'))
         WebDriverWait(self.driver, 3).until(EC.visibility_of(self.driver.find_element(By.CLASS_NAME, 'venueList')))
         # venueItem:qdb venueDetailBottomItem:羽毛球场
@@ -107,7 +141,6 @@ class Counter:
         find_elements(By.CLASS_NAME, 'venueItem')[0].\
             find_element(By.CLASS_NAME, 'venueDetailBottom').\
                 find_elements(By.CLASS_NAME, 'venueDetailBottomItem')[0].click() 
-
         WebDriverWait(self.driver, 3).until(EC.url_to_be('https://epe.pku.edu.cn/venue/pku/venue-reservation/60'))
         # date
         WebDriverWait(self.driver, 3).until(EC.visibility_of(self.driver.find_element(By.CLASS_NAME, 'ivu-icon-ios-calendar-outline')))
@@ -117,7 +150,6 @@ class Counter:
         print('Booting...')
         
 
-    
     def count(self):
         print('Counting...')
 
--- parser.py
+++ parser.py
@@ -7,10 +7,15 @@ class ConfParser:
         conf = ConfigParser()
         conf.read(config, encoding='utf8')
         
+        # 用户信息
         self.id, self.password = dict(conf['user']).values()
 
-        self.venue = dict(conf['venue']).values()
-        self.date = dict(conf['date']).values()
-        self.times, self.len = dict(conf['times']).values()
-        
+        # 场地信息
+        self.date_st, self.date_ed = conf.getint('date', 'st'), conf.getint('date', 'ed')
+        self.times = conf.get('times', 'times')
+        self.timesPriority = self.times.split(' ')
         self.wechat, self.sdkey = dict(conf['wechat']).values()
+        
+
+if __name__ == '__main__':
+    conf = ConfParser('config.ini')
